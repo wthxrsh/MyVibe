0e7e025bd5a49b2f0489998025141771bc10b169
Harsh Shah <wthxrsh@hxrsh.wthxrsh>
2025-02-13 08:55:28 +0530

    latest

--- pinterest_clone/pinterest_clone/settings.py
+++ pinterest_clone/pinterest_clone/settings.py
@@ -56,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'pinterest_clone.urls'
